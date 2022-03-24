b4fae964639c68fb7b6a046d90503263b5fe5b36
yakir barzilai <notcooldudet@gmail.com>
2022-03-24 16:20:56 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+            int i;
+            int average = 0;
+            Console.WriteLine("please enter" + grades.Length + "grades");
+            for (i = 0; i < grades.Length; i++)
+                grades[i] = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < grades.Length; i++)
+
+                for (i = 0; i < grades.Length; i++)
+                    average += grades[i];
+            average = average / grades.Length;
+            Console.WriteLine("your average is :{0}", average);
+        }
+    }
+}
